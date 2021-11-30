99e469c4ae5d36f4036201db27f55939aa957d49
alexz <alexzmuda@gmail.com>
2021-11-30 13:45:45 +0100

    auth

--- app/Modules/Product/Providers/RouteServiceProvider.php
+++ app/Modules/Product/Providers/RouteServiceProvider.php
@@ -49,7 +49,7 @@ class RouteServiceProvider extends ServiceProvider
     {
         Route::middleware('web')
             ->namespace($this->moduleNamespace)
-            ->group(module_path('Product', '/Routes/web.php'));
+            ->group(module_path('Product', '/Routes/api.php'));
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +11,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+    Route::post('/login', [\App\Http\Controllers\AuthController::class, 'login']);
+    Route::post('/register', [\App\Http\Controllers\AuthController::class, 'register']);
+    Route::post('/logout', [\App\Http\Controllers\AuthController::class, 'logout']);
+    Route::post('/refresh', [\App\Http\Controllers\AuthController::class, 'refresh']);
+    Route::get('/me', [\App\Http\Controllers\AuthController::class, 'me']);
+});
+
+Route::group(['middleware' => ['auth:api']], function () {
 });
